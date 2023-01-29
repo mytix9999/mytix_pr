d372995ccfa6676005b7588ae9b46d650c407949
mytix9999 <118541568+mytix9999@users.noreply.github.com>
2023-01-29 01:50:59 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Print package with time'
+LONG_DESCRIPTION = 'A small package for success, failed and normal prints'
+
+# Setting up
+setup(
+    name="mytix",
+    version=VERSION,
+    author="Mytix",
+    author_email="mytixhaha@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['colorama', 'time'],
+    keywords=['python', 'print', 'time', 'fail', 'errors', 'success'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
