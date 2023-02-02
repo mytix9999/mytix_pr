ab6b44c563170f554906ecac5515d620f1a7c805
Mytix <118541568+mytix9999@users.noreply.github.com>
2023-02-03 00:04:17 +0100

    Update example.py

--- example.py
+++ example.py
@@ -1,6 +1,8 @@
-from mytix_pr import pr_success, pr_fail ,pr, pr_warn, pr_input
-pr("something")
-pr_success("success")
-pr_fail("error")
-pr_warn("warning!")
-pr_input("type something:  ")
+from mytix_pr import pr
+
+
+pr.info("mytix_pr 1.0.0")
+pr.warn("warning")
+pr.fail("failed")
+pr.success("success")
+pr.input("Input text: ")
